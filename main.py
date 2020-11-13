6c6ce2b4cd014aeaf75922c9d5fcbe3cef22324e
fincap <finnkerman@gmail.com>
2020-11-14 02:12:07 +1100

    Rudimentary single-file command-line conversion working.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import sys
 from PIL import Image
 
 if __name__ == '__main__':
-    im = Image.open(sys.argv[0])
+    im = Image.open(sys.argv[1])
     rgb_im = im.convert('RGB')
-    rgb_im.save('colors.jpg', quality=75)
+    rgb_im.save('output/colors.jpg', quality=75)
