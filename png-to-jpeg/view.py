ecbbf03dca6dbc0a8a9c02485c58852529bdbd2a
fincap <finnkerman@gmail.com>
2020-11-14 16:18:53 +1100

    Added browse destination button to main view.

--- png-to-jpeg/view.py
+++ png-to-jpeg/view.py
@@ -38,12 +38,25 @@ class MainPage(QtWidgets.QMainWindow):
         layout_slider_quality.addWidget(self.slider_quality)
         layout_slider_quality.addWidget(self.label_quality)
 
+        # Output location
+        label_choose_destination = QtWidgets.QLabel("Choose output destination directory.")
+        widget_output_location = QtWidgets.QWidget()
+        layout_output_location = QtWidgets.QHBoxLayout(widget_output_location)
+        self.text_output_location = QtWidgets.QLineEdit()
+        self.text_output_location.setReadOnly(True)
+        button_output_location = QtWidgets.QPushButton("Browse...")
+        button_output_location.clicked.connect(self.on_output_clicked)
+        layout_output_location.addWidget(self.text_output_location)
+        layout_output_location.addWidget(button_output_location)
+
         # Add widgets to the left panel
         layout_left_panel.addWidget(label_browse_files)
         layout_left_panel.addWidget(button_browse_files)
         layout_left_panel.addWidget(QHLine())
         layout_left_panel.addWidget(label_choose_quality)
         layout_left_panel.addWidget(widget_slider_quality)
+        layout_left_panel.addWidget(label_choose_destination)
+        layout_left_panel.addWidget(widget_output_location)
 
         central_layout.addWidget(left_panel)
         self.setCentralWidget(central_widget)
@@ -56,6 +69,11 @@ class MainPage(QtWidgets.QMainWindow):
 
     def on_slider_updated(self, value):
         self.label_quality.setText(str(value))
+
+    def on_output_clicked(self):
+        filepath = QtWidgets.QFileDialog.getExistingDirectory(self, "Open Directory")
+        if filepath != '':
+            self.text_output_location.setText(filepath)
 
 
 class QHLine(QtWidgets.QFrame):
